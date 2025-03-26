35820ebde78f62005f04827a119037a6287e2c5b
vronik <veronicaquinde@digitech.com.ec>
2025-03-26 11:46:05 -0500

    Carga inicial de archivo de configuracion

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+	const APP_URL="https://adfpedrolarrea.digifutbol.com/";
+	const APP_NAME="ADFPL";
+	const APP_SESSION_NAME="ADFPL";
+
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/Guayaquil");
