c3b26580cdd584c108a298fa284fc9ac09264ba3
vronik <veronicaquinde@digitech.com.ec>
2025-04-25 22:28:27 -0500

    Cambio de nombre de lugar de entrenamiento

--- app/views/content/dashboard-view.php
+++ app/views/content/dashboard-view.php
@@ -301,7 +301,7 @@
 				</div>				
 				<div class="card card-default">
 					<div class="card-header">
-						<h3 class="card-title">LA FLORESTA</h3>
+						<h3 class="card-title">FOREST</h3>
 						<div class="card-tools">
 							<button type="button" class="btn btn-tool" data-card-widget="collapse">
 								<i class="fas fa-minus"></i>
