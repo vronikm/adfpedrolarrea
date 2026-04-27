5f53cba8a62ac26db49bd79fba5b167750481348
vronikm <veronicaquinde@digitech.com.ec>
2026-04-26 21:29:31 -0500

    Quitar botones del datatable

--- app/views/content/imagenesList-view.php
+++ app/views/content/imagenesList-view.php
@@ -231,7 +231,6 @@
 					"previous": "Anterior"
 				}
 			},
-			"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
 		}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 	});
 	</script>
